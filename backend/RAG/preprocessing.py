d49f7087aef8884ea4219c8887ef1be3f89e55e2
Walter Nedov <s4742274@student.uq.edu.au>
2025-07-01 09:32:34 +0800

    RAG branch init, created venv etc

---
+++ backend/RAG/preprocessing.py
@@ -0,0 +1,23 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+file_path = '../../ragdb/training-data/feedback-data/'
+loader = PyPDFLoader(file_path)
+
+pages = loader.load()
+
+print(len(pages))
+print(pages[0].page_content)
+print(pages[0].metadata)
+
+
+recursive_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=20,
+    separators=["\n\n", "\n", ".", " ", ""]
+)
+
+
+chunks = recursive_splitter.split_documents(pages)
+with open('log.txt', 'w') as f:
+    f.write(str(chunks))
