c48906aa3d5ca9b6c078cbbab605d1f29a88d124
cpal <alvinyan819@gmail.com>
2025-07-04 10:02:07 +0800

    Use AuthProvider

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages';
 import SignupPage from './pages/signup';
 import LoginPage from './pages/login';
+import Dashboard from './pages/Dashboard';
 import ForgotPasswordPage from './pages/forgor';
 import CareersPage from './pages/careers';
 import ContactPage from './pages/contact';
@@ -14,18 +17,41 @@ import CatPage from './pages/cat';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/careers" element={<CareersPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/cat" element={<CatPage />} />
-        {/* Add other routes later */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/careers" element={<CareersPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/cat" element={<CatPage />} />
+          
+          {/* Protected Routes */}
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/dashboard" element={
+            <ProtectedRoute requiredUserType="admin">
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/teacher/dashboard" element={
+            <ProtectedRoute requiredUserType="teacher">
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/student/dashboard" element={
+            <ProtectedRoute requiredUserType="student">
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
